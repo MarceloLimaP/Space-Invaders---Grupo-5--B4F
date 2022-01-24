fb699d0d4ea82efccfb32e4300d7b699a47f07e5
MarceloLimaP <marcelo.limapereira@hotmail.com>
2022-01-23 23:18:19 -1100

    Teste

---
+++ test/src/App.js
@@ -0,0 +1,12 @@
+
+import Test from './Test.js'
+
+function App() {
+  return (
+    <div>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ test/src/Test.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react"
+
+
+class Test extends Component {
+
+    render() {
+        return <div>Teste GitHub</div>
+    }
+}
+
+export default Test
